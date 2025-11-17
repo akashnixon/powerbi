9ff512279890a28d71cbaa90ac145e6a399f1031
Joshua Rajaa <joshuajojejo123@gmail.com>
2025-11-17 11:24:58 -0500

    Update MSAL

--- powerbi-secure-portal/src/authConfig.js
+++ powerbi-secure-portal/src/authConfig.js
@@ -1,7 +1,7 @@
 export const msalConfig = {
   auth: {
     clientId: "2450ed46-5a7a-4e17-af0a-d2e4ebd4cdae",
-    authority: "https://login.microsoftonline.com/291710b8-8a8f-4e2f-8a7c-5e53ef2b22cc",
+    authority: "https://login.microsoftonline.com/common",
     redirectUri: window.location.origin,
   },
 };
